a8722f0e9551f28dbdefb8f5f4f100aa1f8e11e5
Ilya Karpuk <ilya@katalysis.io>
2018-07-24 16:24:03 +0200

    fixed reference path

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "RipeMD",
     dependencies: [
-        .Package(url: "https://gitlab.com/katalysis/HexString.git", majorVersion: 0),
+        .Package(url: "https://gitlab.com/katalysis/open-source/HexString.git", majorVersion: 0),
    ]
 )
