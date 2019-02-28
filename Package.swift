7df66a3e88e19bc93c49107bfdac20a0e45d3d7a
Alex Tran Qui <alex@katalysis.io>
2019-02-28 16:36:58 +0100

    move to spm manifest 4.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,16 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "RipeMD",
-    dependencies: [
-        .Package(url: "https://gitlab.com/katalysis/open-source/HexString.git", majorVersion: 0),
-   ]
+  name: "RipeMD",
+  products: [
+    .library(name: "RipeMD", targets: ["RipeMD"])
+    ],
+  dependencies: [
+    .package(url: "https://gitlab.com/katalysis/open-source/HexString.git", from: "0.0.0"),
+    ],
+  targets: [
+    .target(name: "RipeMD", dependencies: ["HexString"], path: ".", sources: ["Sources"]),
+    ]
 )
